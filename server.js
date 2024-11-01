ebd8792c93a6e0e9d580be2f08ab168fe912debb
supa-modo <eddieochieng11@gmail.com>
2024-11-01 11:35:13 +0300

    Modified server.js to listen to all ports instead of just localhost

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ sequelize
   .sync()
   .then(() => {
     console.log("Database connected and synced");
-    app.listen(PORT_URL, () => {
-      // app.listen(PORT_URL, "0.0.0.0", () => {
+    // app.listen(PORT_URL, () => {
+      app.listen(PORT_URL, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT_URL}`);
     });
   })
